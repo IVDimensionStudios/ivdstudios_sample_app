0550b8c5ebd20ab06b158af5df74955e5644d7eb
AbhimanyuAryan <abhimanyu.chat@gmail.com>
2016-11-17 19:29:40 +0000

    To Start Listing 5.35 after Integration Test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,20 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "IVth Dimension Studios"
+    assert_select "title", "IV Dimension Studios"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Help | IVth Dimension Studios"
+    assert_select "title", "Help | IV Dimension Studios"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About | IVth Dimension Studios"
+    assert_select "title", "About | IV Dimension Studios"
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | IV Dimension Studios"
   end
 end
